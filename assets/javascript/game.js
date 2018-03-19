82f619073f7e92d1365fe20615a0a7657c95384f
Caroline Rink <carolinerink@MacBook-Pro-2.local>
2018-03-19 10:04:25 -0400

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,6 +6,7 @@ var underScore = [];
 var rightGuess = ["Right: "];
 var wrongGuess = ["Wrong: "];
 var guessesRemaining = 7;
+var userGuess;
 
 // Choose words randomly
 var randomResponse = bands[Math.floor(Math.random() * bands.length)];
@@ -26,36 +27,47 @@ var generateUnderscores = () => {
     return underScore;
 }
 
-document.addEventListener("keypress", (event) => {
-    var letter = String.fromCharCode(event.keyCode);
-    if (randomResponse.indexOf(letter) > -1) {
+function updateLetters() {
+    for (var j = 0; j < randomResponse.length; j++) {
+        if (randomResponse[j] === userGuess) 
+        underScore[j] = randomResponse[j];
+    }
+}
+
+document.onkeyup = function(event) {
+    userGuess = event.key.toLowerCase();
+    console.log(userGuess);
+    if (randomResponse.indexOf(userGuess) > -1) {
+        updateLetters();
         // Adding right guesses to right guess array
-        rightGuess.push(letter);
+        rightGuess.push(userGuess);
         // Fill letters guessed into underscore spots if they are correct and push to right guess class
-        underScore[randomResponse.indexOf(letter)] = letter;
-        underscoreDom[0].textContent = underScore.join(" ");
-        rightGuessDom[0].textContent = rightGuess;
+        // underScore[randomResponse.indexOf(userGuess)] = userGuess;
+        underscoreDom.textContent = underScore.join(" ");
+        rightGuessDom.textContent = rightGuess;
         
         console.log(underScore);
+        console.log(rightGuess);
 
          // Alert if user guesses right answer
         if (underScore.join(" ") == randomResponse) {
-        alert("You Win!")
+            alert("You Win!")
 
     }
         // Push wrong guesses and guesses remaining into wrong guess and guesses remaining class
     } else { 
-        if (wrongGuess.indexOf(letter) === -1) {
+        if (wrongGuess.indexOf(userGuess) === -1) {
             guessesRemaining--;
-            wrongGuess.push(letter);
-            wrongGuessDom[0].textContent = wrongGuess;
-            guessesRemainingDom[0].textContent = guessesRemaining;
+            wrongGuess.push(userGuess);
+            wrongGuessDom.textContent = wrongGuess;
+            guessesRemainingDom.
+            textContent = guessesRemaining;
         }
 
         console.log(guessesRemaining);
     }
     
-});
+};
 
-underscoreDom[0].textContent = generateUnderscores().join(" ");
+underscoreDom.textContent = underScore.join(" ");
 

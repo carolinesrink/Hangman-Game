e0945491e8e851a7881147dfcaa76f79d4dbb38c
Caroline Rink <carolinerink@MacBook-Pro-2.local>
2018-03-20 11:24:27 -0400

    final changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 // Create an array of musical groups or artists
-const bands = ["fleatwood mac", "the beatles", "the lumineers", "ed sheeran", "james taylor", "prince", "alabama shakes", "arctic monkeys", "the head and the heart", "kendrick lamar", "chance the rapper", "green day", "motion city soundtrack","my chemical romance", "of monsters and men", "panic at the disco", "taylor swift", "twenty one pilots", "the weeknd", "michael jackson", "madonna", "led zeppelin", "pink floyd", "rihanna", "queen", "eminem", "whitney houston", "kanye west", "adele", "journey", "usher", "the black eyed peas"]
+const bands = ["fleatwood mac", "the beatles", "the lumineers", "ed sheeran", "james taylor", "prince", "alabama shakes", "arctic monkeys", "the head and the heart", "kendrick lamar", "chance the rapper", "green day", "motion city soundtrack","my chemical romance", "of monsters and men", "panic at the disco", "taylor swift", "twenty one pilots", "the weeknd", "michael jackson", "madonna", "led zeppelin", "pink floyd", "rihanna", "queen", "eminem", "whitney houston", "kanye west", "adele", "journey", "usher", "the black eyed peas", "childish gambino"]
 
 // Variables
 var underScore = [];
@@ -20,12 +20,15 @@ var wrongGuessDom = document.getElementsByClassName("wrongGuesses");
 var guessesRemainingDom = document.getElementsByClassName("remainingGuesses");
 
 // Create underscores based on words chosen at random
-var generateUnderscores = () => {
+function generateUnderscores() {
     for (var i = 0; i < randomResponse.length; i++) {
-        underScore.push("_");
-    } 
-    return underScore;
+        underScore.push("_"); 
+        if (bands === " ") 
+        underScore.push(" ");
+    }
 }
+
+generateUnderscores();
 
 function updateLetters() {
     for (var j = 0; j < randomResponse.length; j++) {
@@ -42,27 +45,30 @@ document.onkeyup = function(event) {
         // Adding right guesses to right guess array
         rightGuess.push(userGuess);
         // Fill letters guessed into underscore spots if they are correct and push to right guess class
-        // underScore[randomResponse.indexOf(userGuess)] = userGuess;
-        underscoreDom.textContent = underScore.join(" ");
-        rightGuessDom.textContent = rightGuess;
+        underScore[randomResponse.indexOf(userGuess)] = userGuess;
+        underscoreDom[0].textContent = underScore.join(" ");
+        rightGuessDom[0].textContent = rightGuess;
         
         console.log(underScore);
         console.log(rightGuess);
 
          // Alert if user guesses right answer
-        if (underScore.join(" ") == randomResponse) {
-            alert("You Win!")
+        if (userGuess === randomResponse) {
+            alert("You Won!");
+        }
 
-    }
         // Push wrong guesses and guesses remaining into wrong guess and guesses remaining class
     } else { 
         if (wrongGuess.indexOf(userGuess) === -1) {
             guessesRemaining--;
             wrongGuess.push(userGuess);
-            wrongGuessDom.textContent = wrongGuess;
-            guessesRemainingDom.
+            wrongGuessDom[0].textContent = wrongGuess;
+            guessesRemainingDom[0].
             textContent = guessesRemaining;
         }
+            if (guessesRemaining === 0) {
+                alert("You Lose.")
+            }
 
         console.log(guessesRemaining);
     }
@@ -70,4 +76,3 @@ document.onkeyup = function(event) {
 };
 
 underscoreDom.textContent = underScore.join(" ");
-
